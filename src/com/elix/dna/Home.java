342a6b459d74184dcd2443ee779bc53d943c54d7
Steve Kondik <steve@cyngn.com>
2016-09-09 23:55:11 -0700

    DNA: HW Keys custom rebinding and move jdc in dna
    This commit moves the settings of key rebinding and power menu inside dna
    
    Singed-of-by: Alessandro Pasini <alessandro.pasini12@gmail.com>
    
    Change-Id: I0ba91fc3c0a16a49646f269f9cbfd134a2212103

--- src/com/elix/dna/Home.java
+++ src/com/elix/dna/Home.java
@@ -19,6 +19,8 @@ package com.elix.dna;
 
 import com.elix.dna.Device;
 import com.elix.dna.fragments.StatusbarFragment;
+import com.elix.dna.fragments.ButtonFragment;
+import com.elix.dna.fragments.PowerMenuSettings;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import android.app.Activity;
@@ -40,6 +42,8 @@ public class Home extends PreferenceActivity {
     protected boolean isValidFragment(String fragmentName) {
         return Device.class.getName().equals(fragmentName)
 		  || StatusbarFragment.class.getName().equals(fragmentName)
+		  || ButtonFragment.class.getName().equals(fragmentName)
+		  || PowerMenuSettings.class.getName().equals(fragmentName)
 		  || AboutApp.class.getName().equals(fragmentName);
     }
 
